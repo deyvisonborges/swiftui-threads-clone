cf2619e7ef20d94f37183558c7ed830d53f2fe7c
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-08-17 11:25:41 -0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Deyvison Borges on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
